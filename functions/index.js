c4a3c587b5db7fc2428e05d82853745c1f0d52db
aperdomoll90 <aperdomoll90@gmail.com>
2021-04-20 11:09:03 -0400

    updated

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
+const express = require('express')
 const functions = require("firebase-functions");
+const app = express()
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   response.send("Hello from Firebase!");
- });
+app.get('/test',(req,res)=> {
+    res.send('It is working!')
+})
+app.get('/test2',(req,res)=> {
+    res.send('It is working AGAIN!')
+})
+
+ exports.app = functions.https.onRequest(app);
